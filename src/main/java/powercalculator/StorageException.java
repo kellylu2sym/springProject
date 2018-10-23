116bcc2ce088d2b65ba681559349593f2c530709
Lu <Kelly.Lu@emc.com>
2018-10-23 15:56:31 +0800

    add msg

--- src/main/java/powercalculator/StorageException.java
+++ src/main/java/powercalculator/StorageException.java
@@ -1,5 +1,9 @@
 package powercalculator;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.SERVICE_UNAVAILABLE, reason= "db mongoDB could not be reached")
 public class StorageException extends RuntimeException {
 
 	  public StorageException(String message) {
